b112170177691c7433a51ad08dd9875f613e66f5
Nicolas Maltais <nic.maltais@gmail.com>
2020-11-25 17:07:39 -0500

    Add Iris Classification

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
